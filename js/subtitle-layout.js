70fba001b741e3ae179e785e0fb098662e4a9828
ariesandimei-lgtm <ariesandimei@gmail.com>
2026-08-21 18:24:25 +0700

    Update subtitle-layout.js

--- js/subtitle-layout.js
+++ js/subtitle-layout.js
@@ -1,1 +1,85 @@
+class SubtitleLayout {
 
+
+    constructor(){
+
+        this.maxCharacters =
+            42;
+
+        this.maxLines =
+            2;
+
+    }
+
+
+
+    formatWords(words){
+
+
+        let lines = [];
+
+        let currentLine = "";
+
+
+
+        words.forEach(word => {
+
+
+            let testLine =
+                currentLine.length === 0
+                ?
+                word.text
+                :
+                currentLine +
+                " " +
+                word.text;
+
+
+
+            if(
+                testLine.length <=
+                this.maxCharacters
+            ){
+
+                currentLine =
+                    testLine;
+
+            }
+
+            else {
+
+
+                lines.push(
+                    currentLine
+                );
+
+
+                currentLine =
+                    word.text;
+
+
+            }
+
+
+        });
+
+
+
+        if(currentLine){
+
+            lines.push(
+                currentLine
+            );
+
+        }
+
+
+
+        return lines
+            .slice(0,this.maxLines)
+            .join("<br>");
+
+    }
+
+
+}
